17732f7768e9dd4b1355b041e52c655bdc409da4
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-12-02 20:55:29 -0200

    Aula 05 - Adicionando acessórios de carros com toggles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,39 @@
 angular.module('starter')
 .controller('ListagemController', function($scope){
 
-	$scope.listaDeCarros =  ["BMW 120i", "Onix 1.6", "Fiesta 2.0", "C3 1.0", "Uno Fire", "Sentra 2.0",
-								"Astra Sedan", "Vectra 2.0 Turbo", "Hilux 4x4", "Montana Cabine dupla",
-								"Outlander 2.4", "Fusca 1500"];
+  $scope.listaDeCarros =  [
+  {"nome" : "BMW 120i" , "preco" : 70000},
+  {"nome" : "Onix 1.6" , "preco" : 35000},
+  {"nome" : "Fiesta 2.0", "preco" : 52000},
+  {"nome" : "C3 1.0", "preco" : 22000},
+  {"nome" : "Uno Fire", "preco" : 11000},
+  {"nome" : "Sentra 2.0", "preco" : 53000},
+  {"nome" : "Astra Sedan", "preco" : 39000},
+  {"nome" : "Vectra 2.0 Turbo", "preco" : 37000},
+  {"nome" : "Hilux 4x4", "preco" : 90000},
+  {"nome" : "Montana Cabine dupla", "preco" : 57000},
+  {"nome" : "Outlander 2.4" ,"preco" : 99000},
+  {"nome" : "Fusca 1500", "preco" : 6000}
+ ];
 
 });
 
 angular.module('starter')
 .controller('CarroEscolhidoController', function($stateParams, $scope){
-  $scope.carroEscolhido = $stateParams.carro;
+  $scope.carroEscolhido = angular.fromJson($stateParams.carro);
+  $scope.listaDeAcessorios =
+  [
+    {"nome" : "Freio ABS", "preco" : 800},
+    {"nome" : "Ar-condicionado", "preco" : 1000},
+    {"nome" : "MP3 Player", "preco" : 500}
+  ];
+
+  $scope.mudou = function(acessorio, isMarcado){
+    if(isMarcado){
+      $scope.carroEscolhido.preco = $scope.carroEscolhido.preco + acessorio.preco;
+    } else {
+      $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
+    }
+  }
+
 });
